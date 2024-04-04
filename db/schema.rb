abf6fab66b5359c4a89b6e512f149a54512d04e2
meronpandog <j.meronpan.1101@gmail.com>
2024-04-04 03:25:30 +0000

    応用課題4

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_03_31_104544) do
+ActiveRecord::Schema.define(version: 2024_04_03_102847) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -66,6 +66,13 @@ ActiveRecord::Schema.define(version: 2024_03_31_104544) do
   create_table "post_comments", force: :cascade do |t|
     t.text "comment"
     t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
